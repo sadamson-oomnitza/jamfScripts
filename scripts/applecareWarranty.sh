07febdf4cfaabaf5e8500dbdeb3fc19cace88f86
Scott Adamson <96255570+sadamson-oomnitza@users.noreply.github.com>
2025-08-21 13:12:23 -0400

    Update applecareWarranty.sh

--- scripts/applecareWarranty.sh
+++ scripts/applecareWarranty.sh
@@ -7,6 +7,7 @@
 # Last Modified Date: 08-21-25
 # Tested on macOS Version: macOS Ventura - Sequoia
 # Updated for new Apple file type (json)
+# Updated by Scott Adamson (Oomnitza Inc)
 # Contact HCS: info@hcsonline.com
 # Visit HCS: https://hcsonline.com
 ####################################
